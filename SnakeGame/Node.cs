b3ddcf8b6f904b13cf0519717f0d0b67732fdf21
fanjae <hjjang45@gmail.com>
2026-05-04 12:31:55 +0900

    Refactor : Getter,Setter Method Property로 변경

--- SnakeGame/Node.cs
+++ SnakeGame/Node.cs
@@ -2,34 +2,31 @@
 { 
     internal class Node
     {
-        private Position value;
-        Node prev;
-        Node next;
+        private readonly Position nodePos;
+        private Node prev;
+        private Node next;
         public Node(int x, int y)
         {
-            this.value = new Position(x, y);
+            this.nodePos = new Position(x, y);
             prev = null;
             next = null;
         }
-        public void setPrev(Node node) // 이전 위치 설정
+
+        public Node Prev
         {
-            this.prev = node;
+            get { return prev; }
+            set { prev = value; }
         }
-        public void setNext(Node node) // 다음 위치 설정
+
+        public Node Next
         {
-            this.next = node;
+            get { return next; }
+            set { next = value; }
         }
-        public Node getPrev() // 이전값 반환
+
+        public Position NodePos
         {
-            return prev;
-        }
-        public Node getNext() // 다음값 반환
-        {
-            return next;
-        }
-        public Position getPosition() // 위치값 반환
-        {
-            return value;
+            get { return nodePos; }
         }
     }
 }
