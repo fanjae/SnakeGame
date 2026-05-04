d07db3266550379bf49c744ccaa5a31ed935ec2b
fanjae <hjjang45@gmail.com>
2026-05-04 12:29:17 +0900

    Refactor : Getter 메서드 프로퍼티로 변경 및 readonly 추가

--- SnakeGame/Position.cs
+++ SnakeGame/Position.cs
@@ -2,20 +2,15 @@
 {    
     struct Position // 좌표값 구조체
     {
-        private int x;
-        private int y;
+        private readonly int x;
+        private readonly int y;
         public Position(int x, int y)
         {
             this.x = x;
             this.y = y;
         }
-        public int GetX()
-        {
-            return x;
-        }
-        public int GetY()
-        {
-            return y;
-        }
+
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
     }
 }
