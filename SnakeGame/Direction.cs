f247a4de5c9bba4582ab741c499d4336f0b4635d
fanjae <hjjang45@gmail.com>
2026-05-07 22:28:18 +0900

    Chore : 불필요한 함수 정리

--- SnakeGame/Direction.cs
+++ SnakeGame/Direction.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnakeGame
+﻿namespace SnakeGame
 {    
     internal enum Direction { Up = 0, Left = 1, Down = 2, Right = 3 }; // 상하좌우 표기 용도
 }
